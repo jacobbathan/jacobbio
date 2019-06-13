3fe165ddad3fdc679a7519ee859714a394254f1d
jacobbathan <jacobbathan@gmail.com>
2019-06-12 17:45:03 -0700

    trying after runing npm run build

--- src/components/dashboard/About.jsx
+++ src/components/dashboard/About.jsx
@@ -11,26 +11,30 @@ class About extends React.Component {
 
           <div className=''>
             <div className='col-1-of-2'>
-              <h3 className='heading-tertiary u-margin-bottom-small' />
+              {/* <h3 className='heading-tertiary u-margin-bottom-small' /> */}
               <p className='paragraph'>
-                Welcome to my website! My name is Kerry Staley and I am a
-                passionate full stack developer from San Diego, CA. I received
-                my Bachelor of Arts from the University of California, Santa
-                Barbara majoring in Global Studies with an emphasis in Chinese
-                Economics and Environmental Impact. However, my true passion and
-                motivation lies within the tech industry. I found a passion for
-                technology when I was young and taught myself how to code via
-                online courses and my surrounding network. To further my
-                programming experience and repertoire, I worked for an
-                e-commerce start-up company called OutlayR, whose purpose is to
-                connect social media influencers with online merchants. During
-                my time with OutlayR, I worked on a team with 16 other
-                engineers, practicing Agile / SCRUM methodology. The stack that
-                our web-application was built in consisted of ASP.NET Core 2.2,
-                Node.js, React.js and SQL Server Management Studio 2017. Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
-                ipsum sapiente aspernatur libero repellat quis consequatur
-                ducimus quam nisi exercitationem omnis earum qui.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                at porttitor sapien, in venenatis leo. Sed porta nisl in nunc
+                dictum, in dignissim nisi viverra. Donec viverra porta justo,
+                eget finibus turpis semper non. Fusce euismod dui sed maximus
+                sodales. Pellentesque facilisis ipsum nunc, sed feugiat sem
+                volutpat feugiat. Donec pulvinar, purus id bibendum dignissim,
+                tortor turpis ultrices ex, sed blandit est lectus sit amet
+                magna. Curabitur eros leo, rutrum at vulputate eu, ultrices at
+                lacus. Etiam eget aliquam lacus. Praesent condimentum blandit mi
+                eu venenatis. Suspendisse odio nunc, fermentum sed scelerisque
+                et, consectetur in neque. Aliquam in ipsum non diam posuere
+                cursus. Suspendisse potenti. Nullam congue tempor erat, sed
+                sodales odio elementum sit amet. Duis porttitor, risus vel
+                eleifend efficitur, ligula nibh sollicitudin mauris, vel
+                fermentum metus nisl id ipsum. In hac habitasse platea dictumst.
+                Vestibulum lobortis nec nisi sed eleifend. Proin placerat elit
+                id mi consequat, at vestibulum ex sodales. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit. Cras vel condimentum nunc,
+                interdum pellentesque eros. Morbi vitae gravida orci, eu mollis
+                libero. Duis viverra sit amet erat vel rhoncus. Donec
+                pellentesque justo eget diam vehicula, vitae fermentum or
+                pharetra. Donec id ipsum finibus, vulputate augue.
               </p>
               <h3 className='heading-tertiary u-margin-bottom-small'>
                 Technologies I've worked with
