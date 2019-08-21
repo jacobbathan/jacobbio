b4ef284be813e74d7ab4b387187e9f8028ed0313
jacobbathan <jacobbathan@gmail.com>
2019-08-21 10:59:02 -0700

    removed whitespace

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -4,12 +4,11 @@ class Main extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <header className='header' />
-
-        <div className='text-box'>
-          <h1 className='heading-primary'>
-            <span className='heading-primary-main'>Jacob Bathan</span>
-            <span className='heading-primary-sub'>
+        <header className="header" />
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">Jacob Bathan</span>
+            <span className="heading-primary-sub">
               Full Stack Web Developer
             </span>
           </h1>
